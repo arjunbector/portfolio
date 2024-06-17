621462d35a5a9329fed9642ce203798d46cb1700
arjunbector <arjunbector@gmail.com>
2024-06-17 11:40:42 +0530

    feat: add intro section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/sections/HeroSection";
+import IntroSection from "@/components/sections/IntroSection";
 
 export default function Home() {
 
   return (
     <main className="min-h-screen px-10">
      <HeroSection/>
+     <IntroSection/>
     </main>
   );
 }
---
+++ src/components/sections/IntroSection.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MaxWidthWrapper from "../ui/MaxWidthWrapper";
+import TextRevealByWord from "../ui/text-reveal";
+
+const IntroSection = () => {
+  return (
+    <MaxWidthWrapper>
+      <TextRevealByWord text="I craft intuitive, responsive web experiences that seamlessly blend aesthetic design with functional elegance." />
+    </MaxWidthWrapper>
+  );
+};
+
+export default IntroSection;
