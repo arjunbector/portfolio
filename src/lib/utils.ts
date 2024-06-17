68e62fd644d58a754b05b84f4f9db65a058fb2c5
arjunbector <arjunbector@gmail.com>
2024-06-17 16:46:57 +0530

    add metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,27 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function constructMetadata({
+  title = "Arjun Bector",
+  description = "Welcome to my web development portfolio! I'm Arjun Bector, a skilled web developer specializing in full-stack development. Explore my innovative web solutions, responsive designs, and interactive applications built with HTML, CSS, JavaScript, React, Node.js, and more. Check out my projects and see how I can bring your digital vision to life.",
+  image = "/image.jpg",
+  icons = "/favicon.ico"
+}: {
+  title?: string,
+  description?: string,
+  image?: string,
+  icons?: string
+} = {}): Metadata {
+  return {
+    title, description, openGraph: {
+      title, description, images: [{ url: image }]
+    },
+    icons,
+    metadataBase: new URL("https://arjun-bector-portfolio.vercel.app/")
+  }
 }
