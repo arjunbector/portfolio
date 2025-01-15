2130b975ac506ec1e733027c279cf3b89c16a696
arjunbector <arjunbector@gmail.com>
2025-01-15 09:16:01 +0530

    feat: integrate ReactLenis for smooth scrolling experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { Toaster } from "@/components/ui/toaster";
-import { Analytics } from "@vercel/analytics/react"
-import { constructMetadata } from "@/lib/utils";
 import Header from "@/components/Header";
+import { Toaster } from "@/components/ui/toaster";
+import { constructMetadata } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/react";
+import { Inter } from "next/font/google";
+import { ReactLenis } from "@/lib/lenis";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,12 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Header/>
-        <Toaster />
-        {children}
-        <Analytics/>
-      </body>
+      <ReactLenis root>
+        <body className={inter.className}>
+          <Header />
+          <Toaster />
+          {children}
+          <Analytics />
+        </body>
+      </ReactLenis>
     </html>
   );
 }
