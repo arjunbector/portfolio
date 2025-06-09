03b5436a272cd251a537a8f4e6e34a64bb66506e
Arjun Bector <arjunbector@gmail.com>
2025-06-09 18:44:18 +0530

    feat: add LeetCode social media handle to constants

--- src/constants/social-media.ts
+++ src/constants/social-media.ts
@@ -1,4 +1,5 @@
 import { FileText, Github, Linkedin, Mail } from "lucide-react";
+import { TbBrandLeetcode } from "react-icons/tb";
 
 export const SOCIAL_MEDIA_HANDLES = [
   {
@@ -20,5 +21,10 @@ export const SOCIAL_MEDIA_HANDLES = [
     name: "Resume",
     url: "https://drive.google.com/file/d/1OGYIhof7FrvSol3fhUmL9-N3dc3Grmt_/view?usp=sharing",
     icon: FileText
+  },
+  {
+    name: "LeetCode",
+    url: "https://leetcode.com/u/arjunbector/",
+    icon: TbBrandLeetcode
   }
 ];
